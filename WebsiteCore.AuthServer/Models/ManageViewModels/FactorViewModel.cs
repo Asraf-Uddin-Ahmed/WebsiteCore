6321150f0dc8eee94bf3719f0dfad976b1988f2a
asraf uddin ahmed <13ratul@gmail.com>
2017-08-23 16:55:33 +0600

    Controller, model and view update

--- WebsiteCore.AuthServer/Models/ManageViewModels/FactorViewModel.cs
+++ WebsiteCore.AuthServer/Models/ManageViewModels/FactorViewModel.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace WebsiteCore.AuthServer.Models.ManageViewModels
+﻿namespace WebsiteCore.AuthServer.Models.ManageViewModels
 {
     public class FactorViewModel
     {
