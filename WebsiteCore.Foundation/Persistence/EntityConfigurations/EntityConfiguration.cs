fb27e4b5059d7eb0a2b9dba34b42b0b1981ff6e5
asraf uddin ahmed <13ratul@gmail.com>
2017-09-08 12:43:28 +0600

    Base entity configure for EntityTypeConfiguration pattern

---
+++ WebsiteCore.Foundation/Persistence/EntityConfigurations/EntityConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using WebsiteCore.Foundation.Core.Entities;
+using WebsiteCore.Foundation.Microsoft.EntityFrameworkCore;
+
+namespace WebsiteCore.Foundation.Persistence.EntityConfigurations
+{
+    public abstract class EntityConfiguration<TEntity> : EntityTypeConfiguration<TEntity>
+          where TEntity : Entity
+    {
+        public abstract void ConfigureEntity(EntityTypeBuilder<TEntity> builder);
+
+        public override void Configure(EntityTypeBuilder<TEntity> builder)
+        {
+            builder.Property(e => e.Id)
+                .IsRequired();
+
+            ConfigureEntity(builder);
+        }
+    }
+}
