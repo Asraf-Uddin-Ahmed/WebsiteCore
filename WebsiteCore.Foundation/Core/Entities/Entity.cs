12aebe9ab20c543ba080da72241d5db51658ad12
asraf uddin ahmed <13ratul@gmail.com>
2017-09-08 12:41:45 +0600

    Base entity add

---
+++ WebsiteCore.Foundation/Core/Entities/Entity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebsiteCore.Foundation.Core.Entities
+{
+    public abstract class Entity
+    {
+        public Guid Id { get; set; }
+    }
+}
