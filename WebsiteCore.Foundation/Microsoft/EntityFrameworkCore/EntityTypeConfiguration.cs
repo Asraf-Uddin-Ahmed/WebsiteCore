fe7be7fda2cae8db686ed8c5ffcae5b04e5e6fc6
asraf uddin ahmed <13ratul@gmail.com>
2017-09-08 12:40:23 +0600

    EntityTypeConfiguration pattern implement for ef core

---
+++ WebsiteCore.Foundation/Microsoft/EntityFrameworkCore/EntityTypeConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebsiteCore.Foundation.Microsoft.EntityFrameworkCore
+{
+    public abstract class EntityTypeConfiguration<TEntity>
+        where TEntity : class
+    {
+        public abstract void Configure(EntityTypeBuilder<TEntity> builder);
+    }
+}
---
+++ WebsiteCore.Foundation/Microsoft/EntityFrameworkCore/ModelBuilderExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebsiteCore.Foundation.Microsoft.EntityFrameworkCore
+{
+    public static class ModelBuilderExtensions
+    {
+        public static void AddConfiguration<TEntity>(this ModelBuilder modelBuilder, EntityTypeConfiguration<TEntity> configuration)
+            where TEntity : class
+        {
+            configuration.Configure(modelBuilder.Entity<TEntity>());
+        }
+    }
+}
