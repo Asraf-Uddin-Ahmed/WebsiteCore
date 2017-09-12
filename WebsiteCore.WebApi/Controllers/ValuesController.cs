d6ed3d8e42280ff6a540c186546ec80aad831169
asraf uddin ahmed <13ratul@gmail.com>
2017-09-12 17:35:48 +0600

    Action add in ValuesController with role based authorization

--- WebsiteCore.WebApi/Controllers/ValuesController.cs
+++ WebsiteCore.WebApi/Controllers/ValuesController.cs
@@ -3,39 +3,69 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using WebsiteCore.Foundation.Core.Constant;
 
 namespace WebsiteCore.WebApi.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("[controller]")]
     public class ValuesController : Controller
     {
-        // GET api/values
+        [Authorize(Roles = ApplicationRoles.ADMIN)]
+        [Route("admin")]
+        [HttpGet]
+        public string GetForAdmin()
+        {
+            return "This is from Admin";
+        }
+        [Authorize(Roles = ApplicationRoles.DEVELOPER)]
+        [Route("developer")]
+        [HttpGet]
+        public string GetForDeveloper()
+        {
+            return "This is from Developer";
+        }
+        [Authorize(Roles = ApplicationRoles.EMPLOYEE)]
+        [Route("employee")]
+        [HttpGet]
+        public string GetForEmployee()
+        {
+            return "This is from Employee";
+        }
+        [Authorize]
+        [Route("authorize")]
+        [HttpGet]
+        public string GetForAuthorized()
+        {
+            return "This is from Authorized";
+        }
+        // GET values
         [HttpGet]
         public IEnumerable<string> Get()
         {
             return new string[] { "value1", "value2" };
         }
 
-        // GET api/values/5
+        // GET values/5
         [HttpGet("{id}")]
         public string Get(int id)
         {
             return "value";
         }
 
-        // POST api/values
+        // POST values
         [HttpPost]
         public void Post([FromBody]string value)
         {
         }
 
-        // PUT api/values/5
+        // PUT values/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody]string value)
         {
         }
 
-        // DELETE api/values/5
+        // DELETE values/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
